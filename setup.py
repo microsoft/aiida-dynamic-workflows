0499745b7403bc6076dbd44eb5be55740c4336aa
Joseph Weston <josephweston@microsoft.com>
2021-12-09 11:49:18 -0800

    Transfer all static package information to setup.cfg
    Version and 'cmdclass' information needs to remain dynamic
    (in setup.py), as the version is determined from Git tags
    using miniver.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def get_version_and_cmdclass(package_path):
@@ -19,70 +19,11 @@ def get_version_and_cmdclass(package_path):
     return module.__version__, module.cmdclass
 
 
-plugin = "dynamic_workflows"
-pkg = f"aiida_{plugin}"
+version, cmdclass = get_version_and_cmdclass("aiida_dynamic_workflows")
 
-classifiers = """\
-    Development Status :: 2 - Pre-Alpha
-    Intended Audience :: Science/Research
-    Intended Audience :: Developers
-    Programming Language :: Python :: 3 :: Only
-    Topic :: Software Development
-    Topic :: Scientific/Engineering
-    Operating System :: Linux"""
-
-version, cmdclass = get_version_and_cmdclass(pkg)
-
+# All other options are specified in 'setup.cfg'; the version has to be
+# determined dynamically from git tags (using 'miniver'), so it needs
+# to be done here.
 setup(
-    name=pkg,
-    author="Microsoft Quantum",
-    description=(
-        "Aiida plugin for dynamically defining workflows that execute Python functions"
-    ),
-    license="MIT",
-    packages=find_packages("."),
-    version=version,
-    cmdclass=cmdclass,
-    entry_points={
-        "aiida.calculations": [
-            f"{plugin}.PyCalcJob = {pkg}.calculations:PyCalcJob",
-            f"{plugin}.PyMapJob = {pkg}.calculations:PyMapJob",
-            f"{plugin}.merge_remote_arrays = {pkg}.calculations:merge_remote_arrays",
-        ],
-        "aiida.parsers": [
-            f"{plugin}.PyCalcParser = {pkg}.parsers:PyCalcParser",
-            f"{plugin}.PyMapParser = {pkg}.parsers:PyMapParser",
-        ],
-        "aiida.data": [
-            f"{plugin}.PyData = {pkg}.data:PyData",
-            f"{plugin}.PyArray= {pkg}.data:PyArray",
-            f"{plugin}.PyRemoteData = {pkg}.data:PyRemoteData",
-            f"{plugin}.PyRemoteArray = {pkg}.data:PyRemoteArray",
-            f"{plugin}.PyOutline = {pkg}.data:PyOutline",
-            f"{plugin}.PyFunction = {pkg}.data:PyFunction",
-            f"{plugin}.Nil = {pkg}.data:Nil",
-            f"{plugin}.PyException = {pkg}.data:PyException",
-        ],
-        "aiida.node": [
-            f"process.workflow.{plugin}.WorkChainNode = {pkg}.workchains:WorkChainNode",
-        ],
-        "aiida.schedulers": [
-            f"{plugin}.slurm = {pkg}.schedulers:SlurmSchedulerWithJobArray",
-        ],
-        "aiida.workflows": [
-            f"{plugin}.PyWorkChain = {pkg}.workflow:PyWorkChain",
-            f"{plugin}.RestartedPyMapJob = {pkg}.workchains:RestartedPyMapJob",
-            f"{plugin}.RestartedPyCalcJob = {pkg}.workchains:RestartedPyCalcJob",
-        ],
-    },
-    setup_requires=["reentry"],
-    install_requires=[
-        "aiida-core >=2.0.0.a1,<3.0.0",
-        "toolz >=0.11.0,<1.0.0",
-        "cloudpickle >=2.0.0,<3.0.0",
-        "numpy",
-        "graphviz",
-    ],
-    reentry_register=True,
-    classifiers=[c.strip() for c in classifiers.split("\n")],
+    version=version, cmdclass=cmdclass,
 )
